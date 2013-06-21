1d3a88c455b91592bd4334e73f07692656c4ce67
Sean Fisk <sean@seanfisk.com>
2013-06-21 13:34:26 -0600

    Remove redundant INFOPATH ops.

--- .bash.d/sh_common_rc.bash
+++ .bash.d/sh_common_rc.bash
@@ -12,6 +12,10 @@ path_add_hierarchy ~/Library/Python/2.7
 # Add scripts directory.
 PATH=~/bin:$PATH
 
+# See this post for some more info (haha):
+# <http://unix.stackexchange.com/questions/22329/gnu-texinfo-directory-search-method>
+INFOPATH=/usr/local/share/info/emacs:$INFOPATH
+
 # Nuke the dupes.
 path_remove_duplicates PATH
 path_remove_duplicates MANPATH
@@ -19,12 +23,6 @@ path_remove_duplicates INFOPATH
 
 export PATH
 export MANPATH
-export INFOPATH
-
-# See this post for some more info (haha):
-# <http://unix.stackexchange.com/questions/22329/gnu-texinfo-directory-search-method>
-INFOPATH=/usr/local/share/info/emacs:$INFOPATH
-path_remove_duplicates INFOPATH
 export INFOPATH
 
 # Set umask for more privacy
