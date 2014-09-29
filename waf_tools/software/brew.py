d78a2ef3c560da8b1c9597bd905bed4f0afab8eb
Sean Fisk <sean@seanfisk.com>
2014-09-29 10:48:12 -0400

    Homebrew no longer deletes Info pages by default.

--- waf_tools/software/brew.py
+++ waf_tools/software/brew.py
@@ -3,11 +3,3 @@
 
 def configure(ctx):
     ctx.find_program('brew', mandatory=False)
-
-
-def build(ctx):
-    if not ctx.env.BREW:
-        return
-    # Homebrew deletes (Tex)Info manuals unless you bar it from doing so. Heck
-    # yes I want these, I use Emacs!
-    ctx.env.SHELL_ENV['HOMEBREW_KEEP_INFO'] = 'true'
