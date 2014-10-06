3826567f623c18dab59ac16851d75ae5acd5cd09
Sean Fisk <sean@seanfisk.com>
2014-10-06 00:46:07 -0400

    Add --content-disposition as default Wget option.

--- waf_tools/software/download.py
+++ waf_tools/software/download.py
@@ -1,6 +1,18 @@
 """Detect and configure Wget and Aria2."""
 
+from pipes import quote as shquote
+
 
 def configure(ctx):
     ctx.find_program('aria2c', mandatory=False)
     ctx.find_program('wget')  # Wget is mandatory!!!
+
+
+def build(ctx):
+    # Even though Wget is mandatory, still add the wget alias here to keep
+    # things together, and in case that changes.
+    #
+    # Use the --content-disposition flag to allow sites "to describe what the
+    # name of a downloaded file should be."
+    ctx.env.SHELL_ALIASES['wget'] = shquote(
+        ctx.env.WGET) + ' --content-disposition'
