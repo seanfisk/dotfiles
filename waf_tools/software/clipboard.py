966399475023e66612e33db4eaf7605122ede3e9
Sean Fisk <sean@seanfisk.com>
2014-10-18 17:50:30 -0400

    Revise clip/download aliases; add SHA256 alias.

--- waf_tools/software/clipboard.py
+++ waf_tools/software/clipboard.py
@@ -15,6 +15,10 @@ def configure(ctx):
             ctx.env.COPY_COMMAND = ' '.join(base_cmd_list + ['-in'])
             ctx.env.PASTE_COMMAND = ' '.join(base_cmd_list + ['-out'])
 
+    # For the sha sum alias
+    ctx.find_gnu_util('sha256sum', mandatory=False)
+    ctx.find_program('cut', mandatory=False)
+
 
 def build(ctx):
     if not (ctx.env.COPY_COMMAND and ctx.env.PASTE_COMMAND):
@@ -25,9 +29,17 @@ def build(ctx):
 
     # aria2 might be available, wget is always available
     downloader = shquote(ctx.env.ARIA2C or ctx.env.WGET)
-    # Download from clipboard
-    ctx.env.SHELL_ALIASES['dl'] = downloader + ' "$(paste)"'
-    # Download to clipboard
-    ctx.env.SHELL_ALIASES['dltc'] = (
-        shquote(ctx.env.WGET) + ' --no-verbose --output-document=- "$(paste)" '
-        '| copy')
+    # Download URL in clipboard
+    ctx.env.SHELL_ALIASES['dlc'] = downloader + ' "$(paste)"'
+    # Download contents to stdout
+    ctx.env.SHELL_ALIASES['dlo'] = (
+        shquote(ctx.env.WGET) + ' --no-verbose --output-document=-')
+    # Download URL in clipboard and send contents to stdout
+    ctx.env.SHELL_ALIASES['dlco'] = 'dlo "$(paste)"'
+    # Download URL in clipboard and put contents in clipboard
+    ctx.env.SHELL_ALIASES['dlcc'] = 'dlo "$(paste)" | copy'
+    # Download URL in clipboard and do a SHA-256 sum on it (useful for Chef).
+    if ctx.env.SHA256SUM and ctx.env.CUT:
+        ctx.env.SHELL_ALIASES['dlsha'] = (
+            'dlo "$(paste)" | ' + shquote(ctx.env.SHA256SUM) + ' - | ' +
+            shquote(ctx.env.CUT) + " -d' ' -f1 | copy")
