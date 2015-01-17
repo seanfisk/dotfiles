9383449f86097c6bef0ff9ceda4a855465be5617
Sean Fisk <sean@seanfisk.com>
2015-01-17 01:45:34 -0500

    Change how default editors are handled.

--- waf-tools/software/editor.py
+++ waf-tools/software/editor.py
@@ -10,14 +10,19 @@ def configure(ctx):
 
 def build(ctx):
     if ctx.env.EMACSCLIENT:
-        editor = ctx.env.EMACSCLIENT + ['--alternate-editor=']
+        # This variable should be a one-element list, see below for how it's
+        # used.
+        editor = ctx.env.EMACSCLIENT
+        # This variable can have multiple arguments, also see below for how
+        # it's used.
+        editor_maybe_nonblock = ctx.env.EMACSCLIENT + ['--no-wait']
+
         if ctx.env.E_SINK:
             ctx.install_script('e')
         else:
-            ctx.env.SHELL_ALIASES['e'] = ctx.shquote_cmd(
-                ctx.env.EMACSCLIENT + ['--no-wait'])
+            ctx.env.SHELL_ALIASES['e'] = ctx.shquote_cmd(editor_maybe_nonblock)
 
-        # Add Python packages.
+        # Add Python packages for Emacs.
         # Rope is not Python 3-compatible yet.
         ctx.env.PYENV_VIRTUALENV_DEFAULT_PACKAGES += [
             'elpy==1.6.0',
@@ -28,5 +33,29 @@ def build(ctx):
             'pyflakes==0.8.1',
         ]
     else:
-        editor = ctx.env.NANO
-    ctx.env.SHELL_ENV['EDITOR'] = shquote(ctx.shquote_cmd(editor))
+        # Nano doesn't have a non-blocking mode.
+        editor = editor_maybe_nonblock = ctx.env.NANO
+
+    # Most programs that use EDITOR expect it to block. Here are the programs
+    # that I use that I expect to use the EDITOR variable:
+    #
+    # git config -e; git commit
+    # hg commit
+    # crontab -e
+    #
+    # Different programs handle the value differently. For example, git
+    # evaluates the value using shell rules, while crontab can only take a
+    # single argument. For this reason, it's best to stick to a single
+    # argument. If multiple are needed in the future, create a wrapper script.
+    ctx.env.SHELL_ENV['EDITOR'] = shquote(editor[0])
+
+    # Some programs also use VISUAL, but I haven't found a reason to set that
+    # yet. Most programs use that before checking EDITOR, adding yet another
+    # layer of complexity that's currently not needed.
+
+    # Homebrew also recognizes VISUAL and EDITOR, but does not need its editor
+    # to block. We therefore manually override the editor. Homebrew evaluates
+    # the value using shell rules, and does therefore not need a script.
+    if ctx.env.BREW:
+        ctx.env.SHELL_ENV['HOMEBREW_EDITOR'] = shquote(ctx.shquote_cmd(
+            editor_maybe_nonblock))
