496404f2d1d8b3797c382697b2594bfaa0f02b59
Sean Fisk <sean@seanfisk.com>
2015-01-19 09:45:49 -0500

    Add detection and auto-completion for gibo.

---
+++ waf-tools/software/gibo.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Detect and configure gibo."""
+
+import waflib
+
+def configure(ctx):
+    ctx.find_program('gibo', mandatory=False)
+
+def build(ctx):
+    # Auto-completion detection is programmed specifically for Homebrew.
+    if not (ctx.env.GIBO and ctx.env.BREW):
+        return
+
+    gibo_brew_prefix = ctx.cmd_and_log(
+        ctx.env.BREW + ['--prefix', 'gibo'],
+        quiet=waflib.Context.STDOUT).rstrip()
+    ctx.add_shell_rc_node(ctx.root.find_node([
+        gibo_brew_prefix, 'etc', 'bash_completion.d', 'gibo-completion.bash']),
+                          'bash')
+    ctx.add_shell_rc_node(ctx.root.find_node([
+        gibo_brew_prefix, 'share', 'zsh', 'site-functions', '_gibo']), 'zsh')
