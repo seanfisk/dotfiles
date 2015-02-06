2561d040e22a49b92660012db2edd00d6763c34a
Sean Fisk <sean@seanfisk.com>
2015-02-06 01:48:06 -0500

    Improve handling of devpi.
    - Install devpi to default Python.
    - Also install devpi-web.
    - Generate a launchd plist on OS X.

--- waf-tools/software/devpi.py
+++ waf-tools/software/devpi.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Detect and configure devpi."""
 
+from os.path import join
+
 DEVPI_PYPI_URL = 'http://localhost:3141/root/pypi/+simple/'
 
 def configure(ctx):
@@ -9,6 +11,28 @@ def configure(ctx):
 def build(ctx):
     if not ctx.env.DEVPI_SERVER:
         return
+
+    if ctx.env.MACOSX:
+        # Install a launchd plist for devpi on Mac OS X.
+        out_nodes = [
+            ctx.path.find_or_declare(['gen-config', filename]) for filename in
+            [
+                'net.devpi.plist',
+                'supervisor-devpi.conf',
+                'nginx-devpi.conf',
+                'crontab',
+                'devpi.service',
+            ]
+        ]
+        plist_node = out_nodes[0]
+
+        @ctx.rule(target=out_nodes, vars=['DEVPI_SERVER'])
+        def _make_devpi_gen_config(tsk):
+            return tsk.exec_command(tsk.env.DEVPI_SERVER + ['--gen-config'])
+
+        ctx.install_as(
+            join(ctx.env.PREFIX, 'Library', 'LaunchAgents', plist_node.name),
+            plist_node)
 
     # Ugh... this is ugly. Unfortunately double extensions are not fun.
     for relpath_list in [['pip', 'pip.conf'], ['pydistutils.cfg']]:
