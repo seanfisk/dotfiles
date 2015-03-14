55ccc678cf447da53a7e7bf20a1743148937476b
Sean Fisk <sean@seanfisk.com>
2015-03-14 13:03:06 -0400

    Add chef-client alias.

---
+++ waf-tools/software/chef.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Detect and configure Chef."""
+
+from pipes import quote as shquote
+
+def configure(ctx):
+    ctx.find_program('chef-client', var='CHEF_CLIENT', mandatory=False)
+
+def build(ctx):
+    if not ctx.env.CHEF_CLIENT:
+        return
+    # This isn't actually much shorter, but it matches our Windows PowerShell
+    # alias. If we need to pass any "standard" options in the future (like '-A'
+    # on Windows), we can do that here.
+    ctx.env.SHELL_ALIASES['converge'] = shquote(ctx.env.CHEF_CLIENT[0])
