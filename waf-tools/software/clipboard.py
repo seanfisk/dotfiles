80085d03c93751f9a7fe7c23ff496ab7465539c3
Sean Fisk <sean@seanfisk.com>
2015-03-24 14:46:28 -0400

    Change copy/paste aliases to ccopy/cpaste.

--- waf-tools/software/clipboard.py
+++ waf-tools/software/clipboard.py
@@ -22,22 +22,24 @@ def build(ctx):
         return
 
     for alias in ['copy', 'paste']:
-        ctx.env.SHELL_ALIASES[alias] = ctx.shquote_cmd(
+        # Don't name it just 'paste' because there is already a POSIX program
+        # called 'paste'.
+        ctx.env.SHELL_ALIASES['c' + alias] = ctx.shquote_cmd(
             ctx.env[alias.upper() + '_COMMAND'])
 
     # aria2 might be available, wget is always available
     downloader = ctx.shquote_cmd(ctx.env.ARIA2C or ctx.env.WGET)
     # Download URL in clipboard
-    ctx.env.SHELL_ALIASES['dlc'] = downloader + ' "$(paste)"'
+    ctx.env.SHELL_ALIASES['dlc'] = downloader + ' "$(cpaste)"'
     # Download contents to stdout
     ctx.env.SHELL_ALIASES['dlo'] = (ctx.shquote_cmd(
         ctx.env.WGET + ['--no-verbose', '--output-document=-']))
     # Download URL in clipboard and send contents to stdout
-    ctx.env.SHELL_ALIASES['dlco'] = 'dlo "$(paste)"'
+    ctx.env.SHELL_ALIASES['dlco'] = 'dlo "$(cpaste)"'
     # Download URL in clipboard and put contents in clipboard
-    ctx.env.SHELL_ALIASES['dlcc'] = 'dlo "$(paste)" | copy'
+    ctx.env.SHELL_ALIASES['dlcc'] = 'dlo "$(cpaste)" | ccopy'
     # Download URL in clipboard and do a SHA-256 sum on it (useful for Chef).
     if ctx.env.SHA256SUM and ctx.env.CUT:
         ctx.env.SHELL_ALIASES['dlsha'] = (
-            'dlo "$(paste)" | ' + ctx.shquote_cmd(ctx.env.SHA256SUM) +
-            ' - | ' + ctx.shquote_cmd(ctx.env.CUT) + " -d' ' -f1 | copy")
+            'dlo "$(cpaste)" | ' + ctx.shquote_cmd(ctx.env.SHA256SUM) +
+            ' - | ' + ctx.shquote_cmd(ctx.env.CUT) + " -d' ' -f1 | ccopy")
