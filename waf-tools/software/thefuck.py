2b58ca79a57c79e5c877cda19d60426dad69b1fe
Sean Fisk <sean@seanfisk.com>
2015-11-18 15:56:06 -0500

    Remove use of deprecated 'thefuck-alias'.

--- waf-tools/software/thefuck.py
+++ waf-tools/software/thefuck.py
@@ -5,23 +5,25 @@ import subprocess
 from pipes import quote as shquote
 
 def configure(ctx):
-    ctx.find_program('thefuck-alias', var='THEFUCK_ALIAS', mandatory=False)
+    ctx.find_program('thefuck', var='THEFUCK', mandatory=False)
 
 def _make_alias(tsk):
     out_node = tsk.outputs[0]
     shell = out_node.suffix()[1:]
     with open(out_node.abspath(), 'w') as output_file:
-        # thefuck-alias does automatic shell detection, so run it in the
-        # specified shell. Note that we are *not* using e.g., 'bash -c
-        # thefuck-alias', because this [apparently] causes the shell to exec
-        # directly rather than forking, which messes up the parent process
-        # detection code of thefuck-alias. Instead, we are feeding the input
-        # via stdin.
+        # thefuck does automatic shell detection, so run it in the specified
+        # shell. Note that we are *not* using e.g.,
+        #
+        #     bash -c 'thefuck --alias'
+        #
+        # because this [apparently] causes the shell to exec directly rather
+        # than forking, which messes up the parent process detection code of
+        # thefuck. Instead, we are feeding the input via stdin.
         proc = subprocess.Popen(
             tsk.env[shell.upper()], stdin=subprocess.PIPE, stdout=output_file,
             # Allows stdin and stdout to be text instead of bytes.
             universal_newlines=True)
-        proc.communicate(shquote(tsk.env.THEFUCK_ALIAS[0]) + '\n')
+        proc.communicate(shquote(tsk.env.THEFUCK[0]) + ' --alias \n')
     return proc.returncode
 
 def build(ctx):
