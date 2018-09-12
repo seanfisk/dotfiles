bc901a4710339671ed74d8524911f3fec44f4fa3
Sean Fisk <sean@seanfisk.com>
2018-09-12 15:42:04 -0400

    Add aliases for Architects.

--- waf-tools/software/blue_medora.py
+++ waf-tools/software/blue_medora.py
@@ -4,14 +4,23 @@
 from os.path import join
 from shlex import quote as shquote
 
+ARCHITECT_ALIASES = dict(
+    eu='exuno',
+    vr='vrops',
+)
+
 def configure(ctx):
     locs = ctx.osx_app_locations('SelectStarter')
     ctx.env.SELECTSTARTER = locs[0] if locs else False
     ctx.msg('Checking for SelectStarter', ctx.env.SELECTSTARTER)
+    for exe in ARCHITECT_ALIASES.values():
+        ctx.find_program(exe)
 
 def build(ctx):
-    if not ctx.env.SELECTSTARTER:
-        return
-
-    ctx.env.SHELL_ALIASES['ss'] = shquote(join(
-        ctx.env.SELECTSTARTER, 'Contents', 'MacOS', 'SelectStarter'))
+    if ctx.env.SELECTSTARTER:
+        ctx.env.SHELL_ALIASES['ss'] = shquote(join(
+            ctx.env.SELECTSTARTER, 'Contents', 'MacOS', 'SelectStarter'))
+    for alias, exe in ARCHITECT_ALIASES.items():
+        cmd = ctx.env[exe.upper()]
+        if cmd:
+            ctx.env.SHELL_ALIASES[alias] = ctx.shquote_cmd(cmd)
